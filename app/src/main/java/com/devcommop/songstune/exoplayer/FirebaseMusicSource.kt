61733d3c20935ec3d3aabf540cc1ec1c830d26e6
Joaquin144 <vvpp898.comco@gmail.com>
2022-10-13 19:43:43 +0530

    enum class to represent MusicSource states

---
+++ app/src/main/java/com/devcommop/songstune/exoplayer/FirebaseMusicSource.kt
@@ -0,0 +1,13 @@
+package com.devcommop.songstune.exoplayer
+
+class FirebaseMusicSource {
+
+}
+
+//Here we define all states that can represent the MusicSource
+enum class State {
+    STATE_CREATED,
+    STATE_INITIALIZING,
+    STATE_INITIALIZED,
+    STATE_ERROR
+}
